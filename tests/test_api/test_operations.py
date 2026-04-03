576d9fc9cbe0ca92e2d3c029c30758838f447570
ivxmirov <iterum343@yandex.ru>
2026-04-03 20:59:50 +0400

    tests for add income operation have been added

--- tests/test_api/test_operations.py
+++ tests/test_api/test_operations.py
@@ -8,7 +8,7 @@ def test_add_expense_success(db_session, client):
     user = User(login="test")
     db_session.add(user)
     db_session.flush()
-    wallet = Wallet(name="card", balance=200, user_id=user.id)
+    wallet = Wallet(name="card", balance=200, user_id=user.id, currency="RUB")
     db_session.add(wallet)
     db_session.commit()
     db_session.refresh(wallet)
@@ -26,11 +26,10 @@ def test_add_expense_success(db_session, client):
 
     # Assert
     assert response.status_code == 200
-    assert response.json()["message"] == "Expense has been added."
-    assert response.json()["wallet"] == wallet.name
+    assert response.json()["type"] == "expense"
     assert Decimal(str(response.json()["amount"])) == Decimal(50)
-    assert Decimal(str(response.json()["new_balance"])) == Decimal(150)
-    assert response.json()["description"] == "food"
+    assert response.json()["currency"] == "rub"
+    assert response.json()["category"] == "food"
 
 
 def test_add_expense_negative_amount(db_session, client):
@@ -38,7 +37,7 @@ def test_add_expense_negative_amount(db_session, client):
     user = User(login="test")
     db_session.add(user)
     db_session.flush()
-    wallet = Wallet(name="card", balance=200, user_id=user.id)
+    wallet = Wallet(name="card", balance=200, user_id=user.id, currency="RUB")
     db_session.add(wallet)
     db_session.commit()
     db_session.refresh(wallet)
@@ -63,7 +62,7 @@ def test_add_expense_empty_name(db_session, client):
     user = User(login="test")
     db_session.add(user)
     db_session.flush()
-    wallet = Wallet(name="card", balance=200, user_id=user.id)
+    wallet = Wallet(name="card", balance=200, user_id=user.id, currency="RUB")
     db_session.add(wallet)
     db_session.commit()
     db_session.refresh(wallet)
@@ -127,7 +126,7 @@ def test_add_expense_not_enough_money(db_session, client):
     user = User(login="test")
     db_session.add(user)
     db_session.flush()
-    wallet = Wallet(name="card", balance=200, user_id=user.id)
+    wallet = Wallet(name="card", balance=200, user_id=user.id, currency="RUB")
     db_session.add(wallet)
     db_session.commit()
     db_session.refresh(wallet)
@@ -145,3 +144,121 @@ def test_add_expense_not_enough_money(db_session, client):
 
     # Assert
     assert response.status_code == 400
+
+
+def test_add_income_success(db_session, client):
+    # Arrange
+    user = User(login="test")
+    db_session.add(user)
+    db_session.flush()
+    wallet = Wallet(name="card", balance=200, user_id=user.id, currency="RUB")
+    db_session.add(wallet)
+    db_session.commit()
+    db_session.refresh(wallet)
+
+    # Act
+    response = client.post(
+        "api/v1/operations/income",
+        json={
+            "wallet_name": "card",
+            "amount": 50.0,
+            "description": "salary",
+        },
+        headers={"Authorization": f"Bearer {user.login}"},
+    )
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json()["type"] == "income"
+    assert Decimal(str(response.json()["amount"])) == Decimal(50)
+    assert response.json()["currency"] == "rub"
+    assert response.json()["category"] == "salary"
+
+
+def test_add_income_negative_amount(db_session, client):
+    # Arrange
+    user = User(login="test")
+    db_session.add(user)
+    db_session.flush()
+    wallet = Wallet(name="card", balance=200, user_id=user.id, currency="RUB")
+    db_session.add(wallet)
+    db_session.commit()
+    db_session.refresh(wallet)
+
+    # Act
+    response = client.post(
+        "api/v1/operations/income",
+        json={
+            "wallet_name": "card",
+            "amount": -100.0,
+            "description": "salary",
+        },
+        headers={"Authorization": f"Bearer {user.login}"},
+    )
+
+    # Assert
+    assert response.status_code == 422
+
+
+def test_add_income_empty_name(db_session, client):
+    # Arrange
+    user = User(login="test")
+    db_session.add(user)
+    db_session.flush()
+    wallet = Wallet(name="card", balance=200, user_id=user.id, currency="RUB")
+    db_session.add(wallet)
+    db_session.commit()
+    db_session.refresh(wallet)
+
+    # Act
+    response = client.post(
+        "api/v1/operations/income",
+        json={
+            "wallet_name": "     ",
+            "amount": 100.0,
+            "description": "salary",
+        },
+        headers={"Authorization": f"Bearer {user.login}"},
+    )
+
+    # Assert
+    assert response.status_code == 422
+
+
+def test_add_income_wallet_not_exists(db_session, client):
+    # Arrange
+    user = User(login="test")
+    db_session.add(user)
+    db_session.commit()
+
+    # Act
+    response = client.post(
+        "api/v1/operations/income",
+        json={
+            "wallet_name": "card",
+            "amount": 100.0,
+            "description": "salary",
+        },
+        headers={"Authorization": f"Bearer {user.login}"},
+    )
+
+    # Assert
+    assert response.status_code == 404
+
+
+def test_add_income_unauthorized(client):
+    # Arrange
+
+    # Act
+    response = client.post(
+        "api/v1/operations/income",
+        json={
+            "wallet_name": "card",
+            "amount": 100.0,
+            "description": "salary",
+        },
+        headers={"Authorization": "Bearer not_exists"},
+    )
+
+    # Assert
+    assert response.status_code == 401
